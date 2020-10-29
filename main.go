4f7d083c1dea71bb53b577b87482e83f9f66c942
Artem <ashumanov@gmail.com>
2020-10-29 10:42:00 +0300

    Init server.

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"SeaBattle/battle"
+	"SeaBattle/web"
+	"flag"
+	"log"
+)
 
+func main() {
+	listenPort := *flag.Int("port", 8080, "server port")
+
+	model := battle.CreateSeaBattle()
+
+	server := web.NewGinServer(model)
+
+	if err := server.Run(listenPort); err != nil {
+		log.Fatalf("api: failed to start server %v", err)
+	}
 }
--- web/server.go
+++ web/server.go
@@ -1,1 +1,55 @@
 package web
+
+import (
+	"SeaBattle/battle"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type ginServer struct {
+	model *battle.SeaBattleGame
+	engine *gin.Engine
+}
+
+
+func NewGinServer(model *battle.SeaBattleGame) *ginServer {
+	server := &ginServer{
+		model: model,
+	}
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.GET("/create-matrix", server.createMatrix)
+	router.GET("/ship", server.ship)
+	router.GET("/shot", server.shot)
+	router.GET("/clear", server.clear)
+	router.GET("/state", server.state)
+
+	server.engine = router
+	return server
+}
+
+func (s *ginServer) Run(port int) error {
+	return s.engine.Run(fmt.Sprintf(":%d", port))
+}
+
+func (s *ginServer) createMatrix(context *gin.Context) {
+
+}
+
+func (s *ginServer) ship(context *gin.Context) {
+
+}
+func (s *ginServer) shot(context *gin.Context) {
+
+}
+func (s *ginServer) clear(context *gin.Context) {
+
+}
+
+func (s *ginServer) state(context *gin.Context) {
+
+}
+
+
+
